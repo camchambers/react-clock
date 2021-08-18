d4c7f582f5dd763e26c33d40eb3bfdedf7656be8
Cam Chambers <cam_chambers@outlook.com>
2021-08-18 08:12:44 -0400

    Clock now ticks

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -6,6 +6,26 @@ class Clock extends React.Component {
     super(props);
     this.state = {date: new Date()};
   }
+
+  componentDidMount(){
+    // Set up a timer whenever the clock is rendered to the DOM
+    // for the first time (mounted)
+    this.timerId = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount(){
+    // Clear the timer whenever the clock is removed from 
+    // the dom (unmount)
+    clearInterval(this.timerId);
+  }
+
+  tick() {
+    this.setState({ date: new Date() });
+  }
+
   render(){
     return(
       <div>
